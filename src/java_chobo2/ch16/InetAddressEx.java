033c8570ff7e67842fd142238230257cb9d65a88
Min <rukiminangel@naver.com>
2021-02-25 12:15:34 +0900

    수정

--- src/java_chobo2/ch16/InetAddressEx.java
+++ src/java_chobo2/ch16/InetAddressEx.java
@@ -6,28 +6,23 @@ import java.util.Arrays;
 
 public class InetAddressEx {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws java.net.UnknownHostException, UnknownHostException {
 		InetAddress ip = null;
 		InetAddress[] ipArr = null;
 
-		try {
-			ip = InetAddress.getByName("www.naver.com");
-			System.out.println("GetHostName() :" + ip.getHostName());
-			System.out.println("GetHostAddress() :" + ip.getHostAddress());
-			System.out.println("toString() :" + ip.toString());
+		ip = InetAddress.getByName("www.naver.com");
+		System.out.println("GetHostName() :" + ip.getHostName());
+		System.out.println("GetHostAddress() :" + ip.getHostAddress());
+		System.out.println("toString() :" + ip.toString());
 
-			byte[] ipAddr = ip.getAddress();
-			System.out.println("getAddress() :" + Arrays.toString(ipAddr));
+		byte[] ipAddr = ip.getAddress();
+		System.out.println("getAddress() :" + Arrays.toString(ipAddr));
 
-			String result = " ";
-			for (int i = 0; i < ipAddr.length; i++)
-				result += (ipAddr[i] < 0 ? ipAddr[i] + 256 : ipAddr[i]) + ".";
-			System.out.println("getAddress()+256 :" + result);
-			System.out.println();
-
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
+		String result = " ";
+		for (int i = 0; i < ipAddr.length; i++)
+			result += (ipAddr[i] < 0 ? ipAddr[i] + 256 : ipAddr[i]) + ".";
+		System.out.println("getAddress()+256 :" + result);
+		System.out.println();
 
 		try {
 			ip = InetAddress.getLocalHost();
